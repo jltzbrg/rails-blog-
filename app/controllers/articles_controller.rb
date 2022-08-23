f3d0a921927028b34fc994ccffe92bb104a9ad96
Julio Litzenberg <jltzbrg@gmail.com>
2022-08-23 12:00:47 +0300

    Adding the create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(articles_params)
+
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/articles', to: 'articles#create'
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new'
   get '/articles/:id', to: 'articles#show', as: :article
